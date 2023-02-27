c40221b057d66a1a3450e79a4e6f70e5b72921d5
Chbabula <126175655+Chbabula@users.noreply.github.com>
2023-02-27 11:12:46 +0530

    Add files via upload

---
+++ assinmentof_js/objct.js
@@ -0,0 +1,96 @@
+const kiran_family = {
+  father: "Ramarao",
+  mother: "Ramalakishmi",
+  Elder_son: "Ramcharn",
+  older_son: "Prabha",
+  doughter: "kumari",
+  allinfo: function () {
+    return (
+        this.father+ ': father name, '+
+      " " +
+      this.mother +
+      ": mother name, " +
+      this.Elder_son +
+      ": Elder Son," +
+      this.older_son +
+      ": older son," +
+      this.doughter +
+      ": doughter, "
+    );
+  },
+};
+console.log(kiran_family.allinfo());
+
+
+// addition
+// 1).
+let e=15.25
+let n=85.20
+d=(e+n)
+console.log('answer is',d)
+// -----------
+// 2).
+let o=50
+var l= 63.25
+s=(o+l)
+console.log('answer is',s)
+// ---------------
+
+// 3).
+var a=10
+var b=50
+c=(a+b)
+console.log('answer is',c)
+
+// multiplicaton
+// 1).
+a=58
+b=99
+console.log('answer is',a*b)
+// 2).
+let w=50
+var l= 63.25
+s=(w*l)
+console.log('answer is',s)
+// 3).
+let q=15.25
+let h=85.20
+d=(q*h)
+console.log('answer is',d)
+
+// divistion
+// 1).
+let u=50
+var l= 63.25
+s=(u/l)
+console.log('answer is',s)
+
+// 2).
+let k=15.25
+let x=85.20
+d=(k/x)
+console.log('answer is',d)
+
+// 3).
+a=4
+b=6
+console.log('answer is', a/b)
+
+// substration
+
+// 1).
+a=50
+b=30
+console.log('answer is', a-b)
+
+// 2).
+let f=45
+let z=32.5
+console.log(f-z)
+// 3).
+var j=600
+var v=1220
+p= (j-v)
+console.log('answer is', p)
+
+alert(p)
